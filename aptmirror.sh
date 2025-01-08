f0e557abbf20afbf3b1df8498d6435740c492dfe
HYwooo <bryanw@vip.qq.com>
2025-01-08 16:53:17 +0800

    update

--- aptmirror.sh
+++ aptmirror.sh
@@ -1,28 +1,26 @@
 #!/bin/sh
 
+# curl -sSL https://raw.githubusercontent.com/HYwooo/install/master/aptmirror.sh | sh
 # Install coreutils if not already installed
-apt install -y coreutils
+sudo apt install -y coreutils ca-certificates
 
 # Try to access Google with a timeout of 2 seconds
 if timeout 2 curl -s -o /dev/null https://www.google.com; then
-    # If successful (no timeout), do nothing
-    :
+
+    sed -i 's/http:/https:/g' /etc/apt/sources.list.d/ubuntu.sources
 else
     # If timeout, execute action A
     echo -e "\033[1;31mTimeout: Unable to access Google, executing action A\033[0m"
     echo -e "\033[1;34m******************* Setting apt mirror: TSINGHUA & USTC ******************\033[0m"
-
-    # Replace with Tsinghua mirror
-    sed -i 's@//.*archive.ubuntu.com@//mirrors.tuna.tsinghua.edu.cn@g' /etc/apt/sources.list.d/ubuntu.sources
-    sed -i 's/security.ubuntu.com/mirrors.ustc.edu.cn/g' /etc/apt/sources.list.d/ubuntu.sources
-
-    # Update package list and install necessary tools
-    apt update -y --fix-missing
-    apt install -y --no-install-recommends curl wget ca-certificates
-
+    
     # Replace HTTP with HTTPS
     sed -i 's/http:/https:/g' /etc/apt/sources.list.d/ubuntu.sources
-    apt update -y
+    # Replace with Tsinghua mirror
+    sudo sed -i 's@//.*archive.ubuntu.com@//mirrors.tuna.tsinghua.edu.cn@g' /etc/apt/sources.list.d/ubuntu.sources
+    sudo sed -i 's/security.ubuntu.com/mirrors.ustc.edu.cn/g' /etc/apt/sources.list.d/ubuntu.sources
+
+    # Update package list and install necessary tools
+    sudo apt update -y --fix-missing
 
     echo -e "\033[1;32m******************* apt mirror set: TSINGHUA & USTC ******************\033[0m"
 fi
--- install.sh
+++ install.sh
@@ -2,19 +2,19 @@
 # apt update -y && apt install -y curl
 # curl -sSL https://cdn.jsdelivr.net/gh/HYwooo/install@master/install | sh
 ############################################################################################################
-apt install -y gcc g++ rustup 
+apt install -y gcc g++ rustup
 mkdir -p ~/.rustup && rustup default stable
-mkdir -p ~/.cargo && mkdir -p ~/.cargo/env 
+mkdir -p ~/.cargo && mkdir -p ~/.cargo/env
 ############################################################################################################
 apt install -y git zsh fonts-powerline
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" 
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone https://github.com/carloscuesta/materialshell.git ~/.oh-my-zsh/custom/themes/materialshell
 sed -i 's/^ZSH_THEME=.*/ZSH_THEME="materialshell\/materialshell"/' ~/.zshrc
 sed -i 's/^plugins=.*/plugins=(git rust python zsh-autosuggestions zsh-syntax-highlighting)/' ~/.zshrc
 eval "$(curl https://get.x-cmd.com)"
-echo "[ ! -f \"\$HOME/.x-cmd.root/X\" ] || . \"\$HOME/.x-cmd.root/X\" " >> ~/.zshrc
-echo ". \"\$HOME/.cargo/env\"" >> ~/.zshrc && echo "export TERM=xterm-256color" >> ~/.zshrc
+echo "[ ! -f \"\$HOME/.x-cmd.root/X\" ] || . \"\$HOME/.x-cmd.root/X\" " >>~/.zshrc
+echo ". \"\$HOME/.cargo/env\"" >>~/.zshrc && echo "export TERM=xterm-256color" >>~/.zshrc
 chsh -s $(which zsh)
-echo "******************* reconnect to apply changes ******************"  
+echo "******************* reconnect to apply changes ******************"
