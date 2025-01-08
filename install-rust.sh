35888f1930e8ccbf3da51bffdf99bab053933f5d
HYwooo <bryanw@vip.qq.com>
2025-01-08 19:36:06 +0800

    Refactor installation scripts to streamline Rustup setup and add mirror configuration

---
+++ install-rust.sh
@@ -0,0 +1,15 @@
+#!/bin/zsh
+sudo apt install -y coreutils ca-certificates rustup --quiet --no-install-recommends
+if timeout 2 curl -s -o /dev/null https://www.google.com; then
+    rustup default stable
+else
+    echo -e "\033[1;31mTimeout: Unable to access Google.\033[0m"
+    echo -e "\033[1;34m******************* Setting Rustup mirror: TSINGHUA   ******************\033[0m"
+    echo "export RUSTUP_UPDATE_ROOT=https://mirrors.tuna.tsinghua.edu.cn/rustup" >> ~/.zshrc
+    echo "export RUSTUP_DIST_SERVER=https://mirrors.tuna.tsinghua.edu.cn/rustup" >> ~/.zshrc
+    source ~/.zshrc
+    echo -e "\033[1;32m******************* Rustup mirror set:    TSINGHUA    ******************\033[0m"
+    rustup default stable
+    echo -e '[registries.crates-io]\nprotocol = "sparse"\n[source.crates-io]\nreplace-with = "tuna"\n[source.tuna]\nregistry = "sparse+https://mirrors.tuna.tsinghua.edu.cn/crates.io-index/"' >> ~/.cargo/config.toml
+    echo -e "\033[1;32m******************* Cargo mirror set: TSINGHUA SPARSE ******************\033[0m"
+fi
--- install.sh
+++ install.sh
@@ -1,20 +1,14 @@
 #!/bin/sh
-# apt update -y && apt install -y curl
+# apt update -y && apt install -y curl sudo --quiet
 curl -sSL https://gcore.jsdelivr.net/gh/HYwooo/install@master/aptmirror.sh | sudo sh
 ############################################################################################################
-apt install -y gcc g++ rustup
-mkdir -p ~/.rustup && rustup default stable
-mkdir -p ~/.cargo && mkdir -p ~/.cargo/env
+apt install -y rustup --no-install-recommends
+curl -sSL https://gcore.jsdelivr.net/gh/HYwooo/install@master/rustmirror.sh | sudo sh
+rustup default stable
+curl -sSL https://gcore.jsdelivr.net/gh/HYwooo/install@master/cargomirror.sh | sudo sh
 ############################################################################################################
-apt install -y git zsh fonts-powerline
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-syntax-highlighting ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-git clone https://github.com/carloscuesta/materialshell.git ~/.oh-my-zsh/custom/themes/materialshell
-sed -i 's/^ZSH_THEME=.*/ZSH_THEME="materialshell\/materialshell"/' ~/.zshrc
-sed -i 's/^plugins=.*/plugins=(git rust python zsh-autosuggestions zsh-syntax-highlighting)/' ~/.zshrc
 eval "$(curl https://get.x-cmd.com)"
 echo "[ ! -f \"\$HOME/.x-cmd.root/X\" ] || . \"\$HOME/.x-cmd.root/X\" " >>~/.zshrc
-echo ". \"\$HOME/.cargo/env\"" >>~/.zshrc && echo "export TERM=xterm-256color" >>~/.zshrc
+echo ". \"\$HOME/.cargo/env\"" >>~/.zshrc 
 chsh -s $(which zsh)
 echo "******************* reconnect to apply changes ******************"
