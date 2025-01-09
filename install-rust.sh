2b34089ae44339abbc4f87f1b0e30e2b52d4043b
HYwooo <bryanw@vip.qq.com>
2025-01-09 22:19:46 +0800

    Change shell interpreter in install-rust.sh to /bin/sh; add cargo directory creation and update .cargo/env for environment configuration

--- install-rust.sh
+++ install-rust.sh
@@ -1,9 +1,10 @@
-#!/bin/zsh
+#!/bin/sh
 # curl -sSL https://gcore.jsdelivr.net/gh/HYwooo/install@master/install-rust.sh | sudo sh
 sudo apt install -y coreutils ca-certificates gcc g++ --quiet --no-install-recommends
 if timeout 2 curl -s -o /dev/null https://www.google.com; then
     curl https://sh.rustup.rs -sSf | sh -s -- -y
     /usr/bin/zsh -c "rustup default stable"
+    mkdir -p $HOME/.cargo
     echo '. "$HOME/.cargo/env"' >>$HOME/.zshrc
     echo 'export PATH="$HOME/.cargo/bin:$PATH"' >>$HOME/.cargo/env
     /usr/bin/zsh -c "source ~/.zshrc"
@@ -22,6 +23,7 @@ else
     mkdir -p $HOME/.cargo
     echo '. "$HOME/.cargo/env"' >>$HOME/.zshrc
     echo 'export PATH="$HOME/.cargo/bin:$PATH"' >>$HOME/.cargo/env
+    
     echo -e '[registries.crates-io]\nprotocol = "sparse"\n[source.crates-io]\nreplace-with = "tuna"\n[source.tuna]\nregistry = "sparse+https://mirrors.tuna.tsinghua.edu.cn/crates.io-index/"' >>~/.cargo/config.toml
     echo -e "\033[1;32m******************* Cargo mirror set: TSINGHUA SPARSE ******************\033[0m"
     /usr/bin/zsh -c "source ~/.zshrc"
