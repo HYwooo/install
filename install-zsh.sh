24d6b3a44bf29151edce46f2c17bd0c66ae6ffeb
HYwooo <43744594+HYwooo@users.noreply.github.com>
2025-01-10 13:57:55 +0800

    Update install-zsh.sh

--- install-zsh.sh
+++ install-zsh.sh
@@ -4,9 +4,6 @@ echo -e "\033[1;34m******************* Installing Zsh ******************\033[0m"
 # Install Git and Zsh quietly without recommended packages, then set Zsh as the default shell, and create a symbolic link from /bin/sh to Zsh
 sudo apt install -y git zsh --quiet --no-install-recommends && chsh -s $(which zsh) $USER
 
-# Add an environment variable to set Zsh as the default shell in the ~/.zshrc file
-echo "export SHELL=/usr/bin/zsh" >>~/.zshrc
-
 # Print a green-colored header indicating the completion of Zsh installation
 echo -e "\033[1;32m******************* Zsh installed ******************\033[0m"
 
@@ -14,7 +11,7 @@ echo -e "\033[1;32m******************* Zsh installed ******************\033[0m"
 echo -e "\033[1;34m******************* Installing Oh My Zsh ******************\033[0m"
 
 # Install Oh My Zsh by downloading and running the installation script from the official repository
-$(which zsh) -c "$(curl -fsSL https://gcore.jsdelivr.net/gh/ohmyzsh/ohmyzsh@master/tools/install.sh)"
+curl -fsSL https://gcore.jsdelivr.net/gh/ohmyzsh/ohmyzsh@master/tools/install.sh | sh -s -- -y
 
 # Clone the zsh-autosuggestions plugin repository into the Oh My Zsh custom plugins directory
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
