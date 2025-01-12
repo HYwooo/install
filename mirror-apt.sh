8f8a62eb0b30ede760c7862fa14fc535ccb71886
HYwooo <43744594+HYwooo@users.noreply.github.com>
2025-01-12 14:29:07 +0800

    Update mirror-apt.sh

--- mirror-apt.sh
+++ mirror-apt.sh
@@ -1,15 +1,20 @@
 #!/bin/sh
-# TO learn if running in compicated network environment, and set apt mirror accordingly.
-# curl -sSL https://raw.githubusercontent.com/HYwooo/install/master/mirror-apt.sh | sh
+# TO learn if running in complicated network environment, and set apt mirror accordingly.
+# From latest[jsdelivr]:
+# curl -sSL https://gcore.jsdelivr.net/gh/HYwooo/install@latest/mirror-apt.sh | sh
+# From master[raw]:
+# curl -sSL https://raw.githubusercontent.com/HYwooo/install/refs/heads/master/mirror-apt.sh | sh
+# From master[jsdelivr]:
 # curl -sSL https://gcore.jsdelivr.net/gh/HYwooo/install@master/mirror-apt.sh | sh
 
 # Install coreutils if not already installed
 sudo apt install -y curl coreutils ca-certificates --quiet --no-install-recommends
-if [ "$USE_MIRROR" != 1 ]; then
-    echo -e "\033[1;33mUSE_MIRROR not set to 1, skipping mirror setup.\033[0m"
+
+if [ "$USE_MIRROR" != "1" ] && timeout 2 curl -s -o /dev/null https://www.google.com; then
+    echo -e "\033[1;33mUSE_MIRROR not set to 1 and Google is reachable, skipping mirror setup.\033[0m"
     sudo sed -i 's/http:/https:/g' /etc/apt/sources.list.d/ubuntu.sources
 else
-    echo -e "\033[1;33mUSE_MIRROR set to 1, setting up mirror.\033[0m"
+    echo -e "\033[1;33mUSE_MIRROR set to 1 or Google is unreachable, setting up mirror.\033[0m"
     echo -e "\033[1;34m******************* Setting up apt mirror: TSINGHUA & USTC ******************\033[0m"
 
     # Replace HTTP with HTTPS
