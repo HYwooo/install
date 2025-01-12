f34702d8f5228245816f1bf1d5a7dff445d6bf23
HYwooo <43744594+HYwooo@users.noreply.github.com>
2025-01-12 14:13:19 +0800

    Update mirror-apt.sh

--- mirror-apt.sh
+++ mirror-apt.sh
@@ -1,19 +1,18 @@
 #!/bin/sh
 # TO learn if running in compicated network environment, and set apt mirror accordingly.
-# curl -sSL https://raw.githubusercontent.com/HYwooo/install/master/mirror-apt.sh | sudo sh
-# curl -sSL https://gcore.jsdelivr.net/gh/HYwooo/install@master/mirror-apt.sh | sudo sh
+# curl -sSL https://raw.githubusercontent.com/HYwooo/install/master/mirror-apt.sh | sh
+# curl -sSL https://gcore.jsdelivr.net/gh/HYwooo/install@master/mirror-apt.sh | sh
+
 # Install coreutils if not already installed
-sudo apt install -y coreutils ca-certificates --quiet --no-install-recommends
-# Try to access Google with a timeout of 2 seconds
-if [ -z "$USE_MIRROR" ] && timeout 2 curl -s -o /dev/null https://www.google.com; then
-    sed -i 's/http:/https:/g' /etc/apt/sources.list.d/ubuntu.sources
+sudo apt install -y curl coreutils ca-certificates --quiet --no-install-recommends
+if [ "$USE_MIRROR" = "1" ]; then
+    echo -e "\033[1;33mUSE_MIRROR not set to 1, skipping mirror setup.\033[0m"
+    sudo sed -i 's/http:/https:/g' /etc/apt/sources.list.d/ubuntu.sources
 else
-    # If timeout:
-    echo -e "\033[1;31mTimeout: Unable to access Google.\033[0m"
-    echo -e "\033[1;34m******************* Setting apt mirror: TSINGHUA & USTC ******************\033[0m"
+    echo -e "\033[1;34m******************* Setting up apt mirror: TSINGHUA & USTC ******************\033[0m"
 
     # Replace HTTP with HTTPS
-    sed -i 's/http:/https:/g' /etc/apt/sources.list.d/ubuntu.sources
+    sudo sed -i 's/http:/https:/g' /etc/apt/sources.list.d/ubuntu.sources
 
     # Replace with Tsinghua mirror
     sudo sed -i 's@//.*archive.ubuntu.com@//mirrors.tuna.tsinghua.edu.cn@g' /etc/apt/sources.list.d/ubuntu.sources
