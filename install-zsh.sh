263a8d72fe971577708b861ee48c8a695a207ca5
HYwooo <43744594+HYwooo@users.noreply.github.com>
2025-01-17 20:50:15 +0800

    Update install-zsh.sh

--- install-zsh.sh
+++ install-zsh.sh
@@ -12,6 +12,7 @@ echo -e "\033[1;34m******************* Installing Zsh ******************\033[0m"
 # Install Git and Zsh quietly without recommended packages
 if ! command -v zsh >/dev/null 2>&1; then
     sudo apt install -y gawk git zsh --quiet --no-install-recommends
+    touch ~/.zshrc
     echo -e "\033[1;32m******************* Zsh installed ******************\033[0m"
 else
     echo -e "\033[1;33mZsh is already installed, skipping installation.\033[0m"
@@ -32,6 +33,7 @@ echo -e "\033[1;34m******************* Installing Zplug ******************\033[0
 # Install Zplug by downloading and running the installation script from the official repository
 if [ ! -d ~/.zplug ]; then
     curl -sL --proto-redir -all,https https://gcore.jsdelivr.net/gh/zplug/installer@master/installer.zsh | zsh
+    zsh -c ". ~/.zplug/init.zsh"
     echo -e "\033[1;32m******************* Zplug installed ******************\033[0m"
 else
     echo -e "\033[1;33mZplug is already installed, skipping installation.\033[0m"
@@ -40,15 +42,12 @@ fi
 # Print a blue-colored header indicating the start of Zsh configuration
 echo -e "\033[1;34m******************* Configuring Zsh with Zplug ******************\033[0m"
 
-# Create a ~/.zshrc file if it doesn't exist
-touch ~/.zshrc
-
 # Add Zplug configuration to ~/.zshrc
 echo -e "\033[1;34m******************* Add info to ~/.zshrc ******************\033[0m"
     cat << EOF >> ~/.zshrc
 # Zplug configuration
 export ZPLUG_HOME=~/.zplug
-source ~/.zplug/init.zsh
+. ~/.zplug/init.zsh
 
 # History config
 HISTSIZE=1000
@@ -57,12 +56,12 @@ HISTFILE=~/.zsh_history
 
 # Plugins
 # zplug "zsh-users/zsh-completions"
-# zplug "zsh-users/zsh-history-substring-search"
+zplug "zsh-users/zsh-history-substring-search"
 zplug "zsh-users/zsh-autosuggestions"
 zplug "zsh-users/zsh-syntax-highlighting"
 
 # Theme (optional, replace with your preferred theme)
-zplug "carloscuesta/materialshell", as:theme
+zplug "carloscuesta/materialshell", as:theme, use:"materialshell.zsh"
 
 # Install plugins if not already installed
 if ! zplug check; then
@@ -75,9 +74,10 @@ zplug load
 # Environment variables
 export TERM=xterm-256color
 EOF
-    echo -e "\033[1;32m******************* Zsh configured with Zplug ******************\033[0m"
+
+echo -e "\033[1;32m******************* Zsh configured with Zplug ******************\033[0m"
 
 
 # Reload the ~/.zshrc file to apply the changes
-zsh -c "source ~/.zshrc"
+zsh -c ". ~/.zshrc"
 
