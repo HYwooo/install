b0681981b7de45081182dcf2cdb6d98c2539054a
HYwooo <bryanw@vip.qq.com>
2025-01-29 11:43:40 +0800

    Refactor install.sh to remove commented-out commands and streamline installation process

--- install.sh
+++ install.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
 # Pre-install:
-# sudo apt update -y && sudo apt install -y curl ca-certificates sudo --quiet --no-install-recommends
+# apt update -y && sudo apt install -y curl ca-certificates sudo --quiet --no-install-recommends
+# sudo apt update -y && sudo apt install -y curl ca-certificates --quiet --no-install-recommends
 #
 # From latest[jsdelivr]:
 # curl -sSL https://gcore.jsdelivr.net/gh/HYwooo/install@latest/install.sh | sh
@@ -13,17 +14,11 @@
 # Download and execute the mirror-apt.sh script to configure apt mirrors
 curl -sSL https://gcore.jsdelivr.net/gh/HYwooo/install@latest/mirror-apt.sh | sh
 
-# Download and execute the install-zsh.sh script to install Zsh
-curl -sSL https://gcore.jsdelivr.net/gh/HYwooo/install@latest/install-zsh.sh | sh
-
 # Download and execute the install-rust.sh script to install Rust
 curl -sSL https://gcore.jsdelivr.net/gh/HYwooo/install@latest/install-rust.sh | sh
 
 # Evaluate and execute the x-cmd installation script
 eval "$(curl https://get.x-cmd.com)"
-
-# Append a line to .zshrc to source the X file if it exists
-echo '[ ! -f "$HOME/.x-cmd.root/X" ] || . "$HOME/.x-cmd.root/X" ' >>~/.zshrc
 
 # Print a message indicating the installation is complete
 echo "******************* Install Finish. ******************"
